63cd49b6b27d5398108a00617d1241e31d851b9f
muhagussaputra20039 <muhagussaputra20039@gmail.com>
2021-12-27 21:57:04 +0800

    bagian Agus

--- main.cpp
+++ main.cpp
@@ -139,3 +139,109 @@ namespace {
 			}
 	};
 }
+
+Image* loadBMP(const char* filename) {
+	ifstream input;
+	input.open(filename, ifstream::binary);
+	assert(!input.fail() || !"Could not find file");
+	char buffer[2];
+	input.read(buffer, 2);
+	assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
+	input.ignore(8);
+	int dataOffset = readInt(input);
+
+	//Read the header
+	int headerSize = readInt(input);
+	int width;
+	int height;
+	switch(headerSize) {
+		case 40:
+			//V3
+			width = readInt(input);
+			height = readInt(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+			assert(readShort(input) == 0 || !"Image is compressed");
+			break;
+		case 12:
+			//OS/2 V1
+			width = readShort(input);
+			height = readShort(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+			break;
+		case 64:
+			//OS/2 V2
+			assert(!"Can't load OS/2 V2 bitmaps");
+			break;
+		case 108:
+			//Windows V4
+			assert(!"Can't load Windows V4 bitmaps");
+			break;
+		case 124:
+			//Windows V5
+			assert(!"Can't load Windows V5 bitmaps");
+			break;
+		default:
+			assert(!"Unknown bitmap format");
+	}
+
+	//Read the data
+	int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+	int size = bytesPerRow * height;
+	auto_array<char> pixels(new char[size]);
+	input.seekg(dataOffset, ios_base::beg);
+	input.read(pixels.get(), size);
+
+	//Get the data into the right format
+	auto_array<char> pixels2(new char[width * height * 3]);
+	for(int y = 0; y < height; y++) {
+		for(int x = 0; x < width; x++) {
+			for(int c = 0; c < 3; c++) {
+				pixels2[3 * (width * y + x) + c] =
+					pixels[bytesPerRow * y + 3 * x + (2 - c)];
+			}
+		}
+	}
+
+	input.close();
+	return new Image(pixels2.release(), width, height);
+}
+//====================Akhir Men-load Image====================
+
+
+//========================Konversi Vedtor ke Vec3f========================
+#ifndef VEC3F_H_INCLUDED
+#define VEC3F_H_INCLUDED
+
+class Vec3f {
+	private:
+		float v[3];
+	public:
+		Vec3f();
+		Vec3f(float x, float y, float z);
+
+		float &operator[](int index);
+		float operator[](int index) const;
+
+		Vec3f operator*(float scale) const;
+		Vec3f operator/(float scale) const;
+		Vec3f operator+(const Vec3f &other) const;
+		Vec3f operator-(const Vec3f &other) const;
+		Vec3f operator-() const;
+
+		const Vec3f &operator*=(float scale);
+		const Vec3f &operator/=(float scale);
+		const Vec3f &operator+=(const Vec3f &other);
+		const Vec3f &operator-=(const Vec3f &other);
+
+		float magnitude() const;
+		float magnitudeSquared() const;
+		Vec3f normalize() const;
+		float dot(const Vec3f &other) const;
+		Vec3f cross(const Vec3f &other) const;
+};
+
+Vec3f operator*(float scale, const Vec3f &v);
+std::ostream &operator<<(std::ostream &output, const Vec3f &v);
+#endif
