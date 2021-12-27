329ed14fe02b4aa8e3f65a5787fa01402bd02b64
webhub20 <ilmifaizan1112@gmail.com>
2021-12-27 20:50:25 +0800

    kodingan dari ilmi

--- main.cpp
+++ main.cpp
@@ -1,1 +1,141 @@
 //silahkan isi kodingannya dibawah
+#include <stdio.h> // Header File For Standard
+#include <stdlib.h>
+#include <string.h>
+#include <math.h> // Math Library Header File
+#include <GL/glut.h>
+#include <GL/glu.h> // Header File For The GLu32 Library
+#include <GL/gl.h> // Header File For The OpenGL32 Library
+#include <time.h>
+#include <sys/timeb.h>
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+//====================Men-load Image====================
+#ifndef IMAGE_LOADER_H_INCLUDED
+#define IMAGE_LOADER_H_INCLUDED
+//Represents an image
+class Image {
+	public:
+		Image(char* ps, int w, int h);
+		~Image();
+
+		/* An array of the form (R1, G1, B1, R2, G2, B2, ...) indicating the
+		 * color of each pixel in image.  Color components range from 0 to 255.
+		 * The array starts the bottom-left pixel, then moves right to the end
+		 * of the row, then moves up to the next column, and so on.  This is the
+		 * format in which OpenGL likes images.
+		 */
+		char* pixels;
+		int width;
+		int height;
+};
+//Reads a bitmap image from file.
+Image* loadBMP(const char* filename);
+#endif
+
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h) {}
+
+Image::~Image() {
+	delete[] pixels;
+}
+
+namespace {
+	//Converts a four-character array to an integer, using little-endian form
+	int toInt(const char* bytes) {
+		return (int)(((unsigned char)bytes[3] << 24) |
+					 ((unsigned char)bytes[2] << 16) |
+					 ((unsigned char)bytes[1] << 8) |
+					 (unsigned char)bytes[0]);
+	}
+
+	//Converts a two-character array to a short, using little-endian form
+	short toShort(const char* bytes) {
+		return (short)(((unsigned char)bytes[1] << 8) |
+					   (unsigned char)bytes[0]);
+	}
+
+	//Reads the next four bytes as an integer, using little-endian form
+	int readInt(ifstream &input) {
+		char buffer[4];
+		input.read(buffer, 4);
+		return toInt(buffer);
+	}
+
+	//Reads the next two bytes as a short, using little-endian form
+	short readShort(ifstream &input) {
+		char buffer[2];
+		input.read(buffer, 2);
+		return toShort(buffer);
+	}
+
+	//Just like auto_ptr, but for arrays
+	template<class T>
+	class auto_array {
+		private:
+			T* array;
+			mutable bool isReleased;
+		public:
+			explicit auto_array(T* array_ = NULL) :
+				array(array_), isReleased(false) {
+			}
+
+			auto_array(const auto_array<T> &aarray) {
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+
+			~auto_array() {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+			}
+
+			T* get() const {
+				return array;
+			}
+
+			T &operator*() const {
+				return *array;
+			}
+
+			void operator=(const auto_array<T> &aarray) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+
+			T* operator->() const {
+				return array;
+			}
+
+			T* release() {
+				isReleased = true;
+				return array;
+			}
+
+			void reset(T* array_ = NULL) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = array_;
+			}
+
+			T* operator+(int i) {
+				return array + i;
+			}
+
+			T &operator[](int i) {
+				return array[i];
+			}
+	};
+}
